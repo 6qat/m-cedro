68ac649946284f510ed1a0d4f4f9ded0985ecf53
Guilherme C. B. Moreira <ceschiatti@gmail.com>
2025-04-28 22:35:11 -0300

    Add k6 flood test

---
+++ src/websocket-server.flood.k6.ts
@@ -0,0 +1,22 @@
+import ws from "k6/ws";
+import { sleep } from "k6";
+
+// https://grafana.com/docs/k6/latest/set-up/install-k6/
+// k6 run src/websocket-server.flood.k6.ts
+
+export const options = {
+  vus: 500,
+  duration: "20s",
+};
+
+export default function () {
+  const url = "ws://localhost:3000/";
+  ws.connect(url, {}, (socket) => {
+    socket.on("open", () => socket.send("ping"));
+    socket.on("message", (m) => {});
+    socket.setInterval(() => socket.send("ping"), 1000);
+    // Close after 1s so this iteration completes
+    socket.setTimeout(() => socket.close(), 2000);
+  });
+  sleep(1);
+}
